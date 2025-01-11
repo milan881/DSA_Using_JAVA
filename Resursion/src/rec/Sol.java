3a61e2a8cf1f82d322ffa005039d56ab01c380b0
milan <milanraja881@gmail.com>
2025-01-11 23:28:44 +0530

    recursion

--- Resursion/src/rec/Sol.java
+++ Resursion/src/rec/Sol.java
@@ -1,21 +1,13 @@
 package rec;
 //Print 1 To N Without Loop
 public class Sol {
-<<<<<<< HEAD
 	/*
-=======
 	
->>>>>>> 7cd68f556a8e739f93232d584272fd4aecbd8605
 	static int count = 0;
 	static int i = 1;
 	public static void print(int n) {
 		
-		
-<<<<<<< HEAD
 		if(count == n) {
-=======
-		if(count == 5) {
->>>>>>> 7cd68f556a8e739f93232d584272fd4aecbd8605
 			return;
 		} 
 			System.out.println(i);
@@ -66,6 +58,4 @@ public class Sol {
     public static void main(String[] args) {
         print(5); 
     }
-=======
->>>>>>> 7cd68f556a8e739f93232d584272fd4aecbd8605
 }
--- Resursion/src/rec/Sum.java
+++ Resursion/src/rec/Sum.java
@@ -1,12 +1,48 @@
 package rec;
 //Sum of n no
-public class Sum {
 
-	public static void sum(int a , int b) {
+
+public class Sum {
+/*
+	static int sum = 0;
+	static int count = 1;
+	
+	public static void sum(int num) {
+		
+		
+		if(count > num) {
+			return;
+		}
+		sum = sum+count;
+		
+		count++;
+		sum(num);
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		sum(5);
+		System.out.println(sum);
+		
+	}
+*/
+	
+	public static void sum(int num, int count, int sum) {
+		
+		if(count > num) {
+			return;
+		}
+		sum = sum+count;
+		System.out.println(sum);
+		count++;
+		sum(num,count,sum);
 		
 	}
 	
 	public static void main(String[] args) {
+		sum(5,1,0);
 		
 	}
+
 }
